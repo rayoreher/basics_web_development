a212068be306e3b05925fe865d5fdbe9e8c7cdae
Ray Orellana <rayoreher@gmail.com>
2025-05-22 21:00:47 +0200

    feat: added github action for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/basics_web_development/',
+  build: {
+    outDir: 'dist',
+  },
   root: ".",
   server: {
     watch: {
